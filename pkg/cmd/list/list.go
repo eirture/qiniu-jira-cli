5bfb4964984a8a48c59d964021fd851704b29852
Jie Liu <eirture@gmail.com>
2023-12-20 17:13:52 +0800

    refactor(list): query in concurrently

--- pkg/cmd/list/list.go
+++ pkg/cmd/list/list.go
@@ -8,6 +8,7 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/andygrunwald/go-jira"
@@ -76,50 +77,70 @@ func SearchPublishingIssues(cli *jira.Client, githubCli *github.Client, services
 		Fields: []string{"*key"},
 		//MaxResults: 2,
 	})
-	if err != nil {
+	if err != nil || len(issues) == 0 {
 		return
 	}
-	for _, si := range issues {
-		var issue *jira.Issue
-		issue, _, err = cli.Issue.Get(si.Key, &jira.GetQueryOptions{})
-		if err != nil {
-			return
-		}
-		var mergedAt, lastedMergedAt time.Time
-		var merged bool
-		for _, cmt := range issue.Fields.Comments.Comments {
-			if cmt.Author.DisplayName != BotName {
-				continue
+
+	workers := len(issues)
+	var wg sync.WaitGroup
+	ch := make(chan string, len(issues))
+	rch := make(chan *Issue, len(issues))
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for issueKey := range ch {
+				issue, _, err := cli.Issue.Get(issueKey, &jira.GetQueryOptions{})
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "get %s error: %v\n", issueKey, err)
+					continue
+				}
+				var mergedAt, lastedMergedAt time.Time
+				var merged bool
+				for _, cmt := range issue.Fields.Comments.Comments {
+					if cmt.Author.DisplayName != BotName {
+						continue
+					}
+					parts := qiniuBotPRCommentPattern.FindStringSubmatch(cmt.Body)
+					if len(parts) != 4 {
+						continue
+					}
+					owner, repo, pr := parts[1], parts[2], parts[3]
+					prNumber, _ := strconv.Atoi(pr)
+					merged, mergedAt, err = cmdutil.IsPRMerged(githubCli, context.Background(), owner, repo, prNumber)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "check %s pr %s error: %v\n", issue.Key, parts[0], err)
+						continue
+					}
+					if !merged {
+						continue
+					}
+					if lastedMergedAt.IsZero() || lastedMergedAt.Before(mergedAt) {
+						lastedMergedAt = mergedAt
+					}
+				}
+				unpublishedServices := getUnpublishedServices(issue, services)
+				if len(unpublishedServices) == 0 {
+					continue
+				}
+				if !lastedMergedAt.IsZero() {
+					rch <- &Issue{
+						Key:                 issue.Key,
+						MergedAt:            lastedMergedAt,
+						UnpublishedServices: unpublishedServices,
+					}
+				}
 			}
-			parts := qiniuBotPRCommentPattern.FindStringSubmatch(cmt.Body)
-			if len(parts) != 4 {
-				continue
-			}
-			owner, repo, pr := parts[1], parts[2], parts[3]
-			prNumber, _ := strconv.Atoi(pr)
-			merged, mergedAt, err = cmdutil.IsPRMerged(githubCli, context.Background(), owner, repo, prNumber)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "check %s pr %s error: %v\n", issue.Key, parts[0], err)
-				continue
-			}
-			if !merged {
-				continue
-			}
-			if lastedMergedAt.IsZero() || lastedMergedAt.Before(mergedAt) {
-				lastedMergedAt = mergedAt
-			}
-		}
-		unpublishedServices := getUnpublishedServices(issue, services)
-		if len(unpublishedServices) == 0 {
-			continue
-		}
-		if !lastedMergedAt.IsZero() {
-			results = append(results, Issue{
-				Key:                 issue.Key,
-				MergedAt:            lastedMergedAt,
-				UnpublishedServices: unpublishedServices,
-			})
-		}
+		}()
+	}
+	for _, issue := range issues {
+		ch <- issue.Key
+	}
+	close(ch)
+	wg.Wait()
+	close(rch)
+	for r := range rch {
+		results = append(results, *r)
 	}
 	return
 }
